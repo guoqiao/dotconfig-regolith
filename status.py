520346ee9b335ad5765a351cbefbe791d0d48268
Joe Guo <joe.guo@canonical.com>
2020-06-13 21:47:46 +1200

    status.py: refactor
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- status.py
+++ status.py
@@ -2,20 +2,33 @@
 import requests
 import yfinance as yf
 
+def get_url(url, fmt='text'):
+    resp = requests.get(url)
+    if fmt == 'json':
+        return resp.json() if resp.ok else {}
+    return resp.text if resp.ok else ''
+
+
 def get_exchange_rates(base='NZD', currencies=['CNY', 'AUD', 'USD']):
-    resp = requests.get(f'https://api.exchangeratesapi.io/latest?base={base}')
-    result = f'1{base}'
-    if resp.ok:
-        rates = resp.json()['rates']
-        for currency in currencies:
-            result += '={:.2f}{}'.format(rates[currency], currency)
-    return result
 
+    data = get_url(f'https://api.exchangeratesapi.io/latest?base={base}', fmt='json')
 
-def get_yfinance_tickers(tickers='AIR.NZ ANZ.NZ ^NZ50 BTC-USD'):
+    if data:
+        rates = data.get('rates', {})
+        if rates:
+
+            def foreach(currency):
+                return '={:.2f}{}'.format(rates[currency], currency)
+
+            return f'1{base}' + ''.join(map(foreach, currencies))
+
+TICKERS = 'AIR.NZ ANZ.NZ ^NZ50 BTC-USD'
+def get_yfinance_tickers(tickers=TICKERS):
 
     def foreach(ticker):
-        info = ticker.info
+        print(ticker)
+        print(dir(ticker))
+        info = ticker.get_info()
         price = info.get('bid') or info.get('ask') or info.get('open')
         return '{}={:.2f}'.format(info['symbol'], price)
 
@@ -23,15 +36,14 @@ def get_yfinance_tickers(tickers='AIR.NZ ANZ.NZ ^NZ50 BTC-USD'):
 
 
 def get_sina_ticker(ticker='s_sh000001'):
-    resp = requests.get(f'http://hq.sinajs.cn/list={ticker}')
-    if resp.ok:
+    text = get_url(f'http://hq.sinajs.cn/list={ticker}')
+    if text:
         # var hq_str_s_sh000001="上证指数,2920.8968,-22.8557,-0.78,2337229,28902326";
-        text = resp.text
         left = text.index('"')
         right = text.rindex('"')
         assert right - left > 10
-        data = text[left+1:right]
-        name, price, _, percent, _, _ = data.split(',')
+        fields = text[left+1:right]
+        name, price, _, percent, _, _ = fields.split(',')
         return '{}:{}({}%)'.format(name, int(float(price)), percent)
 
 
