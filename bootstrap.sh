d98cdd11dd506ad6376bf3d24253b425beb8d5e4
Joe Guo <joe.guo@canonical.com>
2021-02-15 00:17:52 +1300

    bootstrap.sh: add deps
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@
 set -xue
 
 sudo apt update
-sudo apt install -y python3-dev python3-pip lm-sensors flameshot
-sudo python3 -m pip install i3pystatus
+sudo apt install -y python3-dev python3-pip lm-sensors flameshot libiw-dev
+sudo python3 -m pip install i3pystatus colour basiciw
+sudo python3 -m pip install -IU git+https://github.com/enkore/i3pystatus.git
