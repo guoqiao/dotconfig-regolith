0f33c32efb7cca9c3836f61a9f2d123e1068c2fd
Joe Guo <joe.guo@canonical.com>
2021-02-21 18:28:19 -0500

    bootstrap.sh: add yfinance
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ set -xue
 
 sudo apt update
 sudo apt install -y python3-dev python3-pip lm-sensors flameshot libiw-dev
-sudo python3 -m pip install i3pystatus colour basiciw
+sudo python3 -m pip install i3pystatus colour basiciw yfinance
 sudo python3 -m pip install -IU git+https://github.com/enkore/i3pystatus.git
