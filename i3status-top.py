ae90bbd2c970d8f6f216a6e27367c865548453a6
Joe Guo <joe.guo@canonical.com>
2021-03-08 21:12:08 +1300

    i3status-top.py: rm bitcoin which often get 403
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- i3status-top.py
+++ i3status-top.py
@@ -68,11 +68,6 @@ status.register(
     format='⏳{days}d{hours}h',
 )
 
-status.register(
-    "bitcoin",
-    interval=3600,
-)
-
 # status.register('shell', command=str(HERE/'status.py'), format='{output}', interval=600)
 
 status.run()
