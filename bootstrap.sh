fa5a9ef98dabd68a18ec357cb7a9d89d0f14e176
Joe Guo <joe.guo@canonical.com>
2021-04-18 17:18:57 +1200

    update bootstrap.sh
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 set -xue
 
+sudo add-apt-repository ppa:regolith-linux/release
 sudo apt update
-sudo apt install -y python3-dev python3-pip lm-sensors flameshot libiw-dev
-sudo python3 -m pip install i3pystatus colour basiciw yfinance
+sudo apt install -y regolith-desktop-standard python3-dev python3-pip lm-sensors flameshot libiw-dev
+sudo python3 -m pip install i3pystatus colour basiciw
 sudo python3 -m pip install -IU git+https://github.com/enkore/i3pystatus.git
