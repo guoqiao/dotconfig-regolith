7375113c2a61eb280c8d8f4ee5c819fa4733d0f1
Joe Guo <joe.guo@canonical.com>
2021-04-18 16:51:30 +1200

    bar.py: swap bottom and top load order
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bar.py
+++ bar.py
@@ -22,65 +22,6 @@ args = parser.parse_args()
 mode = args.mode
 status = Status()
 
-if mode in ["one", "top"]:
-
-    # refer to /usr/share/zoneinfo for tz
-    status.register(
-        "clock",
-        format=[
-            ("🇳🇿%Z %m.%d %a %H:%M:%S", "NZ"),
-        ],
-        on_rightclick='gnome-control-center datetime',
-    )
-
-    status.register(
-        "clock",
-        format=[
-            ("🇨🇳%Z %d %a %H:%M", "Asia/Shanghai"),
-        ],
-        on_rightclick='gnome-control-center datetime',
-    )
-
-    status.register(
-        "clock",
-        format=[
-            ("🌎%Z %d %a %H:%M", "UTC"),
-        ],
-        on_rightclick='gnome-control-center datetime',
-    )
-
-    status.register(
-        "disk",
-        path="/",
-        format="💽{used:.0f}G",
-        on_rightclick='baobab',  # Disk Usage Analyzer
-    )
-
-    status.register(
-        "mem",
-        format='📈{used_mem}G',
-        divisor=1024 * 1024 * 1024,
-        on_rightclick='gnome-system-monitor --show-processes-tab',
-    )
-
-    status.register(
-        "load",
-        format='🔥{avg1} {avg5} {avg15}',
-        on_rightclick='gnome-system-monitor --show-processes-tab',
-    )
-
-    status.register(
-        "uptime",
-        format='⏳{days}d{hours}h',
-    )
-
-    status.register(
-        'shell',
-        command='sensors | grep "Package" | cut -d" " -f5',
-        format='🌡️{output}',
-        on_rightclick='gnome-system-monitor --show-processes-tab',
-    )
-
 if mode in ["one", "bottom"]:
 
     status.register(
@@ -150,6 +91,65 @@ if mode in ["one", "bottom"]:
         format='🎧{name}',
         show_disconnected=True,
         on_leftclick='gnome-control-center bluetooth',
+    )
+
+if mode in ["one", "top"]:
+
+    # refer to /usr/share/zoneinfo for tz
+    status.register(
+        "clock",
+        format=[
+            ("🇳🇿%Z %m.%d %a %H:%M:%S", "NZ"),
+        ],
+        on_rightclick='gnome-control-center datetime',
+    )
+
+    status.register(
+        "clock",
+        format=[
+            ("🇨🇳%Z %d %a %H:%M", "Asia/Shanghai"),
+        ],
+        on_rightclick='gnome-control-center datetime',
+    )
+
+    status.register(
+        "clock",
+        format=[
+            ("🌎%Z %d %a %H:%M", "UTC"),
+        ],
+        on_rightclick='gnome-control-center datetime',
+    )
+
+    status.register(
+        "disk",
+        path="/",
+        format="💽{used:.0f}G",
+        on_rightclick='baobab',  # Disk Usage Analyzer
+    )
+
+    status.register(
+        "mem",
+        format='📈{used_mem}G',
+        divisor=1024 * 1024 * 1024,
+        on_rightclick='gnome-system-monitor --show-processes-tab',
+    )
+
+    status.register(
+        "load",
+        format='🔥{avg1} {avg5} {avg15}',
+        on_rightclick='gnome-system-monitor --show-processes-tab',
+    )
+
+    status.register(
+        "uptime",
+        format='⏳{days}d{hours}h',
+    )
+
+    status.register(
+        'shell',
+        command='sensors | grep "Package" | cut -d" " -f5',
+        format='🌡️{output}',
+        on_rightclick='gnome-system-monitor --show-processes-tab',
     )
 
 status.run()
