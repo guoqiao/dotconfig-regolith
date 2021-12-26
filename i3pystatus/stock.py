952c884399c08c5563b41c8d0bedae7b352a0b0e
Joe Guo <joe.guo@canonical.com>
2021-12-26 22:35:00 +1300

    stock.py: show nzd to cny exchange rate

--- i3pystatus/stock.py
+++ i3pystatus/stock.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import requests
 from os import getenv
 from binance.client import Client
 
@@ -12,10 +13,20 @@ def get_current_price(symbol):
     return resp.get('price', '0.0').split('.', maxsplit=1)[0]
 
 
+def get_exchange_rate(q="NZD_CNY"):
+    url = f"https://free.currconv.com/api/v7/convert?q={q}&compact=ultra&apiKey=10ad16149788c3c11c6b"
+    resp = requests.get(url)
+    data = resp.json()  # {"NZD_CNY":4.340477}
+    rate_str = data.get(q, '0')
+    rate = float(rate_str)
+    return round(rate, 2)
+
+
 def main():
     btc = get_current_price('BTCUSDT')
     eth = get_current_price('ETHUSDT')
-    print('btc:{}|eth:{}'.format(btc, eth))
+    nzd = get_exchange_rate()
+    print('btc:{}|eth:{}|nzd:{}'.format(btc, eth, nzd))
 
 
 if __name__ == "__main__":
